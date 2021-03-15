744e50dbaab7a02eeb9ece594c83c0ab314e2fd0
Mgm-source <55754333+Mgm-source@users.noreply.github.com>
2021-03-15 20:07:39 +0000

    fixed element-plus setup

--- spatial-webapp/src/main.js
+++ spatial-webapp/src/main.js
@@ -2,8 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import installElementPlus from './plugins/element'
-
-const app = createApp(App)
-installElementPlus(app)
-app.use(store).use(router).mount('#app')
+import ElementPlus from 'element-plus'
+const app = createApp(App);
+app.use(store).use(router).use(ElementPlus).mount('#app')
