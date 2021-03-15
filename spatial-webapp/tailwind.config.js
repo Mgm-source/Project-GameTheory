14805254716f0a29a97c42ea0725a634522f70ca
Mgm-source <55754333+Mgm-source@users.noreply.github.com>
2021-03-15 21:05:29 +0000

    update to tailwind config

--- spatial-webapp/tailwind.config.js
+++ spatial-webapp/tailwind.config.js
@@ -1,18 +1,11 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
-  purge: [
-    './public/**/*.html',
-    './src/**/*.vue',
-  ],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    fontFamily: {
-      sans: ['Inter', ...defaultTheme.fontFamily.sans],
-    },
+    extend: {},
   },
-  variants: {},
+  variants: {
+    extend: {},
+  },
   plugins: [],
-};
+}
