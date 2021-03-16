5399b169b363cc2de5a0b5c00f9a2fea2d4f427a
Mgm-source <55754333+Mgm-source@users.noreply.github.com>
2021-03-16 21:14:23 +0000

    added game router to the link on the parent

--- spatial-webapp/src/App.vue
+++ spatial-webapp/src/App.vue
@@ -5,6 +5,7 @@
             <div class="flex space-x-4">
               <router-link class="bg-gray-600 text-white px-3 py-2 rounded-md text-sm font-medium" :to="{name : 'Home' }">Home</router-link>
               <router-link class="bg-blue-400 text-white px-3 py-2 rounded-md text-sm font-medium" :to="{name : 'About' }">About</router-link>
+              <router-link class="bg-green-500 text-white px-3 py-2 rounded-md text-sm font-medium" :to="{name : 'Game' }">Game</router-link>
             </div>
       </div>
     </div>
