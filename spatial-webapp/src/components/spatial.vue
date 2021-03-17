5e2ac6c0880ad7fd3a5ff197b8ac9263e6a94080
Mgm-source <55754333+Mgm-source@users.noreply.github.com>
2021-03-17 17:19:09 +0000

    initialising dynamic matrix

---
+++ spatial-webapp/src/components/spatial.vue
@@ -0,0 +1,41 @@
+<template>
+  <button class="bg-white hover:bg-blue-500 text-blue-700 font-semibold
+                 hover:text-white py-2 px-4 border border-blue-500" @click="initGame">
+    test
+ </button>
+</template>
+
+<script>
+export default {
+  name: "SpatialGameLogic",
+  components: {},
+
+  data() {
+    return { game: [] };
+  },
+  methods: {
+    createMatrix(x, y) {
+      let gameMatrix = [];
+      let count = 0;
+      for (let i = 0; i < y; i++) {
+        let row = [];
+        for (let j = 0; j < x; j++) {
+          row.push({ strategy: "", payOff: 0, id: count++ });
+        }
+        gameMatrix.push(row);
+      }
+      return gameMatrix;
+    },
+    initGame() {
+      this.game = new this.createMatrix(4, 4);
+      console.log(this.game);
+      this.game.forEach((d,i)=>{
+          d.forEach((a)=>console.log(a,i))
+        })
+    },
+  },
+};
+</script>
+
+<style>
+</style>
