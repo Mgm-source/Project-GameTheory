411dd56b0c686c67745227e1606cb3055e63b842
Mgm-source <55754333+Mgm-source@users.noreply.github.com>
2021-03-17 18:32:17 +0000

    adding the spatial game component to the view

--- spatial-webapp/src/views/Game.vue
+++ spatial-webapp/src/views/Game.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="bg-green-500">
     <h1>This is where the spatial game goes</h1>
+    <spatial></spatial>
   </div>
 </template>
 
 <script>
+import Spatial from '../components/spatial.vue'
 // @ is an alias to /src
 
 export default {
   name: 'Game',
   components: {
+    Spatial
   }
 }
 </script>
