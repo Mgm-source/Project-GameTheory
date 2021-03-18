b37f614079c40070c974a007aa9a167f5c5e3890
Mgm-source <55754333+Mgm-source@users.noreply.github.com>
2021-03-18 03:05:40 +0000

    testing feature t1

--- spatial-webapp/src/components/spatial.vue
+++ spatial-webapp/src/components/spatial.vue
@@ -1,8 +1,17 @@
 <template>
-  <button class="bg-white hover:bg-blue-500 text-blue-700 font-semibold
-                 hover:text-white py-2 px-4 border border-blue-500" @click="initGame">
+ <div v-if="size.x >= 2 && size.y >= 2">
+       <button class="bg-white hover:bg-blue-500 text-blue-700 font-semibold
+                 hover:text-white py-2 px-4 border border-blue-500" @click="initGame" v-if="visible">
     test
  </button>
+ <div v-for="player in players" :key="player.id">
+    <input type="text" v-model="player.strategy"> Id: {{player.id}}
+ </div>
+ </div>
+ <div v-else>
+     X : <input type="text" v-model="size.x"> 
+     Y : <input type="text" v-model="size.y">
+ </div>
 </template>
 
 <script>
@@ -11,7 +20,7 @@ export default {
   components: {},
 
   data() {
-    return { game: [] };
+    return { game: [], players : [], size : {x : 0, y: 0 } , visible : true};
   },
   methods: {
     createMatrix(x, y) {
@@ -26,12 +35,16 @@ export default {
       }
       return gameMatrix;
     },
-    initGame() {
-      this.game = new this.createMatrix(4, 4);
+    initGame(event) {
+        console.log(event);
+        this.visible = !this.visible;
+      this.game = this.createMatrix(this.size.x, this.size.y);
       console.log(this.game);
-      this.game.forEach((d,i)=>{
-          d.forEach((a)=>console.log(a,i))
-        })
+      this.game.forEach(m=>m.forEach(a=>{
+          this.players.push(a);
+          })
+          )
+          console.log(this.players);
     },
   },
 };
